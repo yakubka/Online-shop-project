69f48bba27baab0e80300cb572ee597acda7ae77
Yokubjon <yokubjon_nurullaev@mail.ru>
2025-09-02 08:30:16 +0900

    Update login.php

--- Online-shop-main/searching/login.php
+++ Online-shop-main/searching/login.php
@@ -44,7 +44,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 $conn->close();
 ?>
-
+// HTML Starts here
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -70,3 +70,4 @@ $conn->close();
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></script>
 </body>
 </html>
+
